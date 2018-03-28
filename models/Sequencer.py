a59240bb08303f3974a40de0f7b571aaa1ce4197
Lisa Bonheme <lisa.bonheme@epitech.eu>
2018-03-28 17:10:40 +0200

    Fix sequencer
    - Hard fix the limit of sentence length
    - Switch feature/sequence order in yielded numpy
    - Remove the second (forgotten ?) to_categorical of output_seq
    transformation

--- models/Sequencer.py
+++ models/Sequencer.py
@@ -1,4 +1,3 @@
-import itertools
 import numpy as np
 from keras.preprocessing.sequence import pad_sequences
 from keras.utils import to_categorical
@@ -6,7 +5,7 @@ from commons.utils import load_pickle_file
 
 
 class Sequencer:
-    def __init__(self, captions, features, tokenizer, batch_size):
+    def __init__(self, captions, features, tokenizer, batch_size, max_len=40):
         """
         Load the cleaned captions, features, tokenizer needed for sequence
         generation and set the batch size of the generator as batch_size
@@ -14,21 +13,14 @@ class Sequencer:
         :param features: file containing the dictionary of features
         :param tokenizer: file containing the tokenizer
         :param batch_size: size of each batch yield by generate_sequences
+        :param max_len: max length of a sentence
         """
         self.captions = load_pickle_file(captions)
         self.features = load_pickle_file(features)
         self.tokenizer = load_pickle_file(tokenizer)[1]
-        self.max_len = self._get_max_caption_length()
+        self.max_len = max_len
         self.vocab_len = len(self.tokenizer.word_index) + 1
         self.batch_size = batch_size
-
-    def _get_max_caption_length(self):
-        """
-        Get the longest caption size from all captions
-        :return: maximum caption size
-        """
-        captions = list(itertools.chain.from_iterable(self.captions.values()))
-        return max([len(c) for c in captions])
 
     def generate_sequences(self):
         """
@@ -54,6 +46,6 @@ class Sequencer:
                                                    maxlen=self.max_len)[0]
                         output_seq = to_categorical([seq[i]],
                                                     num_classes=self.vocab_len)[0]
-                        res[0].append(padded_seq)
-                        res[2].append(to_categorical(output_seq))
-                        res[1].append(self.features[img_id])
+                        res[0].append(self.features[img_id][0])
+                        res[1].append(padded_seq)
+                        res[2].append(output_seq)
