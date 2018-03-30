969e45c8cc774a86a63f3477ae90711799b35e09
Lisa Bonheme <lisa.bonheme@epitech.eu>
2018-03-30 03:07:35 +0200

    Improve model evaluation
    - Use variable len of sentence boundary to increase readability
    - Tiny typo fix

--- models/model_eval.py
+++ models/model_eval.py
@@ -5,7 +5,7 @@ from numpy import argmax
 
 def generate_description(model, tokenizer, features, max_len=50):
     input_text = 'startseq'
-    reverse_word_index = {v:k for k,v in tokenizer.word_index.items()}
+    reverse_word_index = {v: k for k, v in tokenizer.word_index.items()}
     for i in range(max_len):
         sequence = tokenizer.texts_to_sequences([input_text])[0]
         sequence = pad_sequences([sequence], maxlen=max_len)
@@ -15,17 +15,15 @@ def generate_description(model, tokenizer, features, max_len=50):
         if word is None or word == 'endseq':
             break
         input_text += ' ' + word
-    return input_text[9:]
+    return input_text[len('startseq'):]
 
 
 def get_bleu_score(y_true, y_pred):
-    bleu_scores = []
-    bleu_scores.append(corpus_bleu(y_true, y_pred, weights=(1.0, 0, 0, 0)))
-    bleu_scores.append(corpus_bleu(y_true, y_pred, weights=(0.5, 0.5, 0, 0)))
-    bleu_scores.append(corpus_bleu(y_true, y_pred, weights=(0.3, 0.3, 0.3, 0)))
-    bleu_scores.append(corpus_bleu(y_true, y_pred,
-                                   weights=(0.25, 0.25, 0.25, 0.25)))
-    bleu_scores = bleu_scores[::-1]
+    bleu_scores = [corpus_bleu(y_true, y_pred, weights=(1.0, 0, 0, 0)),
+                   corpus_bleu(y_true, y_pred, weights=(0.5, 0.5, 0, 0)),
+                   corpus_bleu(y_true, y_pred, weights=(0.3, 0.3, 0.3, 0)),
+                   corpus_bleu(y_true, y_pred, weights=(0.25, 0.25, 0.25, 0.25))
+                   ]
     print("Bleu-1\tBleu-2\tBleu-3\tBleu-4")
     print("{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}".format(*bleu_scores))
 
@@ -33,9 +31,11 @@ def get_bleu_score(y_true, y_pred):
 def evaluate_model(model, captions, features, tokenizer, max_len=50):
     y_true = []
     y_pred = []
+    start = len('startseq')
+    end = -len('endseq')
     for img_id, caption_list in captions.items():
         y = generate_description(model, tokenizer, features[img_id], max_len)
-        refs = [c[9:-7].split() for c in caption_list]
+        refs = [c[start:end].split() for c in caption_list]
         y_true.append(refs)
         y_pred.append(y.split())
     get_bleu_score(y_true, y_pred)
