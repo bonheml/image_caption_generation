337003619e05b0afcd3e6f69420b23dddcae1604
Lisa Bonheme <lisa.bonheme@epitech.eu>
2018-03-30 03:10:06 +0200

    Refactor merge model
    - Use the complete infrastructure of merge model proposed by
    Tanti et al. (2017)

--- models/merge_model.py
+++ models/merge_model.py
@@ -1,49 +1,48 @@
 from keras import Input, Model
-from keras.layers import Dropout, Dense, Embedding, LSTM, add
+from keras.layers import (Dropout, Dense, Embedding, GRU, BatchNormalization,
+                          concatenate)
 
 from commons.utils import load_pickle_file
 
 
-def build_merge_model(embedding, features_shape):
+def build_merge_model(tokenizer, features_shape):
     """
-    Build merge model from Marc Tanti, et al. in their 2017 papers
+    Model based on merge model from Marc Tanti, et al. in their 2017 papers
 
     # References:
     Where to put the Image in an Image Caption Generator, 2017.
     What is the Role of Recurrent Neural Networks (RNNs) in an Image Caption
     Generator?, 2017.
 
-    :param embedding: pickle file containing embedding matrix and tokenizer
+    :param tokenizer: pickle file containing the tokenizer
     :param features_shape: shape of the features extracted from the images
     :return: Merge model
     """
-    embedding_matrix, tokenizer = load_pickle_file(embedding)
-    vocab_size = len(tokenizer.word_index)
+    tokenizer = load_pickle_file(tokenizer)
+    vocab_size = len(tokenizer.word_index) + 1
+    layer_size = 128
 
-    # Features block
-    features_input = Input(shape=features_shape, name="features_input")
-    features_dropout = Dropout(0.5, name='features_dropout')(features_input)
-    features_dense = Dense(256, activation='relu',
-                           name='features_dense')(features_dropout)
+    # Image block
+    image_input = Input(shape=features_shape, name='image_input')
+    image_norm = BatchNormalization(input_shape=features_shape,
+                                    name='image_normalisation')(image_input)
+    image_output = Dense(layer_size, name='image_output')(image_norm)
 
-    # Sequence block
-    sequence_input = Input(shape=(40,), name='sequence_input')
-    sequence_embedding = Embedding(vocab_size, embedding_matrix.shape[1],
-                                   weights=[embedding_matrix], mask_zero=True,
-                                   name='sequence_embedding')(sequence_input)
-    sequence_dropout = Dropout(0.5, name='sequence_dropout')(sequence_embedding)
-    sequence_lstm = LSTM(256, name='sequence_lstm')(sequence_dropout)
+    # Text generation block
+    lang_input = Input(shape=(50,), name='sequence_input')
+    lang_embedding = Embedding(vocab_size, layer_size, mask_zero=True,
+                               input_length=50,
+                               name='sequence_embedding')(lang_input)
+    lang_gru = GRU(layer_size, name='sequence_gru')(lang_embedding)
+    lang_output = Dropout(0.5, name='sequence_output')(lang_gru)
 
     # Merge block
-    merge_add = add([features_dense, sequence_lstm], name='merge_add')
-    merge_dense = Dense(256, activation='relu', name='merge_dense')(merge_add)
-    merge_output = Dense(vocab_size, activation='softmax',
-                         name='merge_output')(merge_dense)
+    wrapper_merge = concatenate([image_output, lang_output],
+                                name='concatenation_layer')
+    wrapper_output = Dense(vocab_size, activation='softmax',
+                           name='word_prediction')(wrapper_merge)
 
-    # Wrap it into a model taking [features, sequence] as parameter and having
-    # word index as output
-    model = Model(inputs=[features_input, sequence_input],
-                  outputs=merge_output)
+    model = Model(inputs=[image_input, lang_input], outputs=wrapper_output)
     model.compile(loss='categorical_crossentropy', optimizer='adam')
 
     return model
